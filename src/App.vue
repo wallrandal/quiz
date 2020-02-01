9f51895ba7252c92c7206a40d3bac35038f6d3cd
Wallace Randal <wmoura@bionexo.com>
2020-02-01 19:28:15 -0300

    front end quiz

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Quiz />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Quiz.vue'
+import './assets/styles/style.sass';
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Quiz
   }
 }
 </script>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="exercise">
+    <div class="header">
+      <div class="fighter">You</div>
+      <div class="fighter">Monster</div>
+    </div>
+    <div class="body">
+      <div class="life-bars">
+        <div class="life-bar"></div> 
+        <div class="life-bar"></div>
+      </div>
+      <div class="start-game">
+        <span>start new game</span>
+      </div>
+      <div class="controls">
+        <div class="attact">
+          <span>Attack</span>
+        </div>
+        <div class="special-attact">
+          <span>Special attack</span>
+        </div>
+        <div class="heal">
+          <span>Heal</span>
+        </div>
+        <div class="give-up">
+          <span>Give up</span>
+        </div>
+      </div>
+      <div class="history">
+        <div class="event">Something has happened</div>
+        <div class="event">Something has happened</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz',
+  data: function() {
+    return {
+     
+    }
+  },
+}
+</script>
+<style lang="sass" scoped>
+  
+</style>
+
+
