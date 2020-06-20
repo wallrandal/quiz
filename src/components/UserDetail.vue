4a6eb27334809dd3544acb8617586ef4be0dde47
Wallace Randal <wmoura@bionexo.com>
2020-06-20 08:40:07 -0300

    changing parent variables through  event

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User name: {{ switchName() }}</p>
+        <button @click="resetName()" >Reset name</button>
     </div>
 </template>
 
@@ -14,6 +15,10 @@
         methods: {
             switchName() {
                 return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = "arara";
+                this.$emit('nameWasReset', this.name);
             },
         },
     }
