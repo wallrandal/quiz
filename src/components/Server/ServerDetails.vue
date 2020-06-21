ea98eef3356d116c14f412b70b781ed980f0f9ad
Wallace Randal <wmoura@bionexo.com>
2020-06-21 17:43:47 -0300

    first version

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="Object.keys(server).length === 0">Server Details are currently not updated</p>
+        <p v-else>{{ server }}</p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+               
+            };
+        },
+        props: {
+            server: {
+                type: Object,
+                required: true,
+            }
+        },
+        methods: {
+
+        },
+    }
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Servers from './Servers.vue'
 
-Vue.component('servers', Servers)
+export const eventBus = new Vue({});
 
-export const eventBus = new Vue({
-  methods: {
-    changeAge (age) {
-      this.$emit('editAge', age)
-    }
-  }
-});
-
-//parent 
 new Vue({
   el: '#app',
   render: h => h(App)
