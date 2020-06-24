ec29150ac182b9779866117b6a9b83d608ad294c
Wallace Randal <wmoura@bionexo.com>
2020-06-24 07:44:15 -0300

    learning about slots

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue({
-  methods: {
-    changeServer (server) {
-      this.$emit('changeServer', server);
-    },
-    switchStatus () {
-      this.$emit('siwtchStatus');
-    },
-  },
-});
-
 new Vue({
   el: '#app',
   render: h => h(App)
