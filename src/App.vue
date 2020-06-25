348385fc1ca7c7d865dbc45da6acb1a63d305697
Wallace Randal <wmoura@bionexo.com>
2020-06-25 09:44:20 -0300

    slots and dynamic components exercise

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,39 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote>
-                    <h2 slot="title"> {{ quoteTitle }} </h2>
-                    <p slot="subtitle">Subtitle coming from outside</p>
-                </app-quote>
+                <br>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p slot="text">This is some text.</p>
+                    </component>
+                </keep-alive>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote';
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
 
     export default {
         data () {
             return {
-                quoteTitle: 'The Quote',
+                selectedComponent: 'appBlue',
             };
         },
         components: {
-            appQuote: Quote,
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
         }
     }
 </script>
 
 <style>
-    h2 {
-        color: pink;
-    }
 </style>
