2ef650f97cfe808ad4dab5381da25206b1574185
Wallace Randal <wmoura@bionexo.com>
2020-06-28 09:39:54 -0300

    finished Quotes app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes" ></app-header>
         <new-quote @quoteAdded="addQuote"></new-quote>
         <hr>
-        <quote-grid :quotes="quotes"></quote-grid>
+        <quote-grid @quoteDeleted="deleteQuote" :quotes="quotes"></quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Delete on a quote to delete it!  
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid';
     import NewQuote from './components/NewQuote';
+    import AppHeader from './components/Header';
 
     export default {
         data () {
@@ -22,10 +31,17 @@
         components: {
             QuoteGrid,
             NewQuote,
+            AppHeader,
         },
         methods: {
             addQuote (quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete quotes first.');
+                }
                 this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
             },
         },
     }
