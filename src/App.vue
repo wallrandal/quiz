5638be1ef86f928b3adb01643d4bbb40c0733a63
Wallace Randal <wmoura@bionexo.com>
2020-06-29 08:57:29 -0300

    first part of the form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,135 @@
 <template>
     <div class="container">
-        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes" ></app-header>
-        <new-quote @quoteAdded="addQuote"></new-quote>
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                v-model="userData.email"
+                                id="email"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                v-model="userData.password"
+                                id="password"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                v-model.number="userData.age"
+                                id="age"
+                                class="form-control">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            v-model="message"
+                            rows="5"
+                            class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    v-model="sendMail"
+                                    name="sendMail"
+                                    id="sendmail"
+                                    value="SendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    v-model="sendMail"
+                                    name="sendInfomail"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                v-model="gender"
+                                id="male"
+                                value="Male"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                v-model="gender"
+                                id="female"
+                                value="Female"> Female
+                    </label>
+                    <label for="other">
+                        <input
+                                type="radio"
+                                v-model="gender"
+                                id="other"
+                                value="other"> Other
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control">
+                        <option></option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
         <hr>
-        <quote-grid @quoteDeleted="deleteQuote" :quotes="quotes"></quote-grid>
         <div class="row">
-            <div class="col-sm-12 text-center">
-                <div class="alert alert-info">
-                    Info: Delete on a quote to delete it!  
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{userData.email}}</p>
+                        <p>Password: {{userData.password}}</p>
+                        <p>Age: {{userData.age}}</p>
+                        <p style="white-space: pre" >Message: {{message}}</p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul v-for="(send, index) in sendMail" :key="index" >
+                            <li> {{send}} </li>
+                        </ul>
+                        <p>Gender: {{gender}} </p>
+                        <p>Priority:</p>
+                        <p>Switched:</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -15,37 +137,22 @@
 </template>
 
 <script>
-    import QuoteGrid from './components/QuoteGrid';
-    import NewQuote from './components/NewQuote';
-    import AppHeader from './components/Header';
-
     export default {
         data () {
             return {
-                maxQuotes: 10,
-                quotes: [
-                    'Just a Quote to see something',
-                ],
+                userData: {
+                    email: '',
+                    password: '',
+                    age: 26,
+                },
+                message: 'a new text',
+                sendMail: [],
+                gender: '',
             };
-        },
-        components: {
-            QuoteGrid,
-            NewQuote,
-            AppHeader,
-        },
-        methods: {
-            addQuote (quote) {
-                if (this.quotes.length >= this.maxQuotes) {
-                    return alert('Please delete quotes first.');
-                }
-                this.quotes.push(quote);
-            },
-            deleteQuote(index) {
-                this.quotes.splice(index, 1);
-            },
         },
     }
 </script>
 
 <style>
+
 </style>
