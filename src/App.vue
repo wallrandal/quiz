15994632b23c359120f9346b35ea6be6840dda35
Wallace Randal <wmoura@bionexo.com>
2020-07-03 16:50:32 -0300

    finished module

--- src/App.vue
+++ src/App.vue
@@ -2,74 +2,35 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Signup Form</h4>
-                    </div>
-                    <form action="">
-                        <div class="panel-body">
-                            <label for="first_name">First Name</label>
-                            <input v-model="firstName" class="form-control" type="text" name="first_name" id="first_name">
-                            
-                            <label for="last_name">Last Name</label>
-                            <input v-model="lastName" class="form-control" type="text" name="last_name" id="last_name">
-                            
-                            <label for="email">E-mail</label>
-                            <input v-model="email" class="form-control" type="email" name="email" id="email">
-                            
-                            <label for="password">Password</label>
-                            <input v-model="password" class="form-control" type="password" name="password" id="password">
-                            
-                            <label for="switch">Store data?</label>
-                            <app-switch id="switch" v-model="switched"></app-switch>
-
-                            <button @click.prevent="submitted = true" type="submit">Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div v-show="submitted" class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: {{fullName}} </p>
-                        <p>Mail: {{email}} </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: {{switched}} </p>
-                    </div>
-                </div>
+                <h1>My custom Directive</h1>
+                <button v-destiny:click="sayMyName" class="btn btn-success">say my name, say my name (click me)</button>
+               
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Switch from './components/Switch';
+
     export default {
-        data () {
-            return {
-                firstName: '',
-                lastName: '',
-                email: '',
-                password: '',
-                switched: true,
-                submitted: false,
-            };
+        methods: {
+            sayMyName() {
+                alert('when no one is around you');
+                return;
+            }
         },
-        components: {
-            AppSwitch: Switch,
-        },
-        computed: {
-            fullName() {
-                return `${this.firstName} ${this.lastName}`;
+        directives: {
+            'destiny': {
+                bind(el, binding) {
+                    let type = binding.arg;
+                    let myFunction = binding.value;
+                    el.addEventListener(type, myFunction);
+                },
             }
         },
     }
 </script>
 
 <style>
+
 </style>
