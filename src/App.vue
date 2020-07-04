9f6ccd2a3dd5fa695488a8865213409395614710
Wallace Randal <wmoura@bionexo.com>
2020-07-04 11:18:20 -0300

    learned about mixins and

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,37 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>My custom Directive</h1>
-                <button v-destiny:click="sayMyName" class="btn btn-success">say my name, say my name (click me)</button>
-               
+                <h1>Filters & Mixins</h1>
+                <!-- Exercise 1) -->
+                <!-- Build a local Filter which reverses the Text it is applied on -->
+                <p> text base: {{text}} </p>
+                <p> reversed: {{text | reverse }} </p>
+                <!-- Exercise 2 -->
+                <!-- Build a global Filter which counts the length of a word and it appends it -->
+                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <p> length count: {{text | textLength }} </p>
+                <!-- Exercise 3 -->
+                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <p> computed reversed: {{ reversed }} </p>
+                <p> computed length count: {{ withCounted }} </p>
+                <!-- Exercise 4 -->
+                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
+    import { stringManipulationMixin } from "./stringManipulationMixin";
     export default {
-        methods: {
-            sayMyName() {
-                alert('when no one is around you');
-                return;
+        filters: {
+            reverse (value) {
+                return value.split("").reverse().join("");
             }
         },
-        directives: {
-            'destiny': {
-                bind(el, binding) {
-                    let type = binding.arg;
-                    let myFunction = binding.value;
-                    el.addEventListener(type, myFunction);
-                },
-            }
-        },
+        mixins: [ stringManipulationMixin ],
     }
 </script>
 
 <style>
-
 </style>
