f50b00643d7e07b2971e861e0bbd7167de8a84da
Wallace Randal <wmoura@bionexo.com>
2020-07-07 11:24:58 -0300

    my version of the quiz

--- src/App.vue
+++ src/App.vue
@@ -1,210 +1,69 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-                <hr>
-                <select class="form-control" name="" v-model="alertAnimation" id="">
-                    <option value="fade">Fade</option>
-                    <option value="slide">Slide</option>
-                </select>
-                <br>
-                <button @click="show = !show" class="btn btn-primary">Show Alert</button>
-                <br><br>
-                <transition :name="alertAnimation">
-                    <div class="alert alert-info" v-if="show">This is some Info</div>
-                </transition>
-                <transition name="slide" type="animation">
-                    <div class="alert alert-info" v-if="show">This is some Info</div>
-                </transition>
-                <transition name="external"
-                            enter-active-class="animate__animated animate__bounce"
-                            leave-active-class="animate__animated animate__shakeX"
-                 appear>
-                    <div class="alert alert-info" v-if="show">This is some Info</div>
-                </transition>
-                <transition :name="alertAnimation" :mode="'out-in'">
-                    <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
-                    <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
-                </transition>
-                <hr>
-                <button class="btn btn-primary" @click="load = !load">Load/ Remove Element</button>
-                <br><br>
-                <transition
-                    @before-enter="beforeEnter"
-                    @enter="enter"
-                    @after-enter="afterEnter"
-                    @enter-cancelled="enterCancelled"
-
-                    @before-leave="beforeLeave"
-                    @leave="leave"
-                    @after-leave="afterLeave"
-                    @leave-cancelled="leaveCancelled"
-                    :css="false"
-                    >
-                    <div style="width: 300px;height: 100px;background-color: lightgreen;" 
-                   v-if="load"></div>
-                </transition>
-                <hr>
-                <button class="btn btn-primary"
-                        @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' :  selectedComponent = 'app-success-alert'  ">
-                    Select alert
-                </button>
-                <br><br>
-                <transition name=fade :mode="'out-in'">
-                    <component :is="selectedComponent"></component>
-                </transition>
-                <hr>
-                <button class="btn btn-primary"
-                        @click="addItem">Add item</button>
-                        <br><br>
-                <ul class="list-group" >
-                    <transition-group name="slide">
-                        <li class="list-group-item" 
-                            v-for="(number, index) in numbers" 
-                            @click="removeItem(index)"
-                            style="cursor:pointer"
-                            :key="index">
-                            {{number}}
-                        </li>
-                    </transition-group>
-                </ul>
-            </div>
+            <h1>The Super Quiz</h1>
         </div>
+        <hr>
+        <div class="row" style="width: 48rem; margin: 0 auto">
+            <transition-group name="my-custom-flip" 
+                        enter-active-class="animate__animated animate__flipInY"
+                        mode="out-in"
+            >
+                <div :key="'in'" class="card" style="width: 48rem; margin: 0 auto" v-if="!selectedRightAnswer">
+                    <div class="card-header">
+                        What's 27 - 24 ?
+                    </div>
+                    <div class="card-body" >
+                        <div class="row col-12 d-flex justify-content-around">
+                            <button @click="answer(-4)" class="btn btn-primary">-4</button>
+                            <button @click="answer(3)" class="btn btn-primary">3</button>
+                        </div>
+                        <br>
+                        <div class="row col-12 justify-content-around">
+                            <button @click="answer(12)" class="btn btn-primary">12</button>
+                            <button @click="answer(14)" class="btn btn-primary">14</button>
+                        </div>
+                    </div>
+                </div>
+                <div :key="'out'" class="card text-white bg-success" style="width: 48rem; margin: 0 auto" v-else>
+                    <div class="card-body" >
+                        <div class="row col-12 d-flex justify-content-around">
+                        <h3>That's correct!</h3>
+                        </div>
+                        <br>
+                        <div class="row col-12 justify-content-around">
+                            <button @click="selectedRightAnswer = !selectedRightAnswer" class="btn btn-primary">Next Question</button>
+                        </div>
+                    </div>
+                </div>
+            </transition-group>
+        </div>
+        
     </div>
 </template>
 
 <script>
     /* eslint no-console: ["error", { allow: ["log","warn", "error"] }] */
-    import DandgerAlert from "./components/DangerAlert";
-    import SuccessAlert from "./components/SuccessAlert";
     export default {
         data() {
             return {
-                show: true,
-                alertAnimation: 'fade',
-                load: true,
-                elementWidth: 100,
-                selectedComponent: 'app-success-alert',
-                numbers: [1,2,3,4,5,],
+                rightAnswer: 27-24,
+                selectedRightAnswer: false,
             };
         },
         methods: {
-            addItem() {
-                const pos = Math.floor(Math.random() * this.numbers.length);
-                this.numbers.splice(pos, 0, this.numbers.length + 1);
-            },
-            removeItem(index) {
-                this.numbers.splice(index, 1);
-            },
-            beforeEnter(el) {
-                console.log('BeforeEnter', el);
-                this.elementWidth = 100;
-                el.style.width = this.elementWidth + 'px';
-            },
-            enter(el, done) {
-                console.log('enter', el);
-                let round = 1;
-                const interval = setInterval(() => {
-                    el.style.width = (this.elementWidth + round * 10) + 'px';
-                    round++;
-                    if(round> 20) {
-                        clearInterval(interval);
-                        done();
-                    }
-                }, 20);
-            },
-            afterEnter(el) {
-                console.log('afterEnter', el);
-            },
-            enterCancelled(el) {
-                console.log('enterCancelled', el);
-            },
-            beforeLeave(el) {
-                console.log('BeforeLeave', el);
-                this.elementWidth = 300;
-                el.style.width = this.elementWidth + 'px';
-                // el.style.backgroundColor = 'yellow';
-            },
-            leave(el, done) {
-                console.log('leave', el);
-                let round = 1;
-                const interval = setInterval(() => {
-                    el.style.width = (this.elementWidth - round * 10) + 'px';
-                    round++;
-                    if(round> 20) {
-                        clearInterval(interval);
-                        done();
-                    }
-                }, 20);
-            },
-            afterLeave(el) {
-                console.log('afterLeave', el);
-            },
-            leaveCancelled(el) {
-                console.log('leaveCancelled', el);
-            },
-        },
-        components: {
-            appDangerAlert: DandgerAlert,
-            appSuccessAlert: SuccessAlert,
+            answer (selectedNumber) {
+                if(Number(selectedNumber) !== Number(this.rightAnswer)) {
+                    alert('sorry, wrong answer');
+                } else {
+                    this.selectedRightAnswer = true;
+                }
+            }
         }
     }
+    
 </script>
 
 <style>
-    .fade-enter {
-        opacity: 0;
-    }
-    
-    .fade-enter-active {
-        transition: opacity 1s;
-    }
-    
-    .fade-leave {
-        opacity: 1;
-    }
-    
-    .fade-leave-active {
-        transition: opacity 1s;
-        opacity: 0;
-    }
-    
-    .slide-enter {
-        transform: translateY(20px);
-        opacity: 0;
-    }
-    
-    .slide-enter-active {
-        animation: slide-in 1s ease-out forwards;
-        transition: opacity 0.5s;
-    }
-    
-    .slide-leave-active {
-        animation: slide-out 1s ease-out forwards;
-        transition: opacity 1;
-        /* position:absolute; */
-    }
-    
-    .slide-move {
-        transition: transform 1s;
-    }
 
-    @keyframes slide-in {
-        from {
-            transform: translateY(20px);
-        }
-        to {
-            transform: translateY(0);
-        }
-    }
-
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-        }
-        to {
-            transform: translateY(20px);
-        }
-    }
 </style>
