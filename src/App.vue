418db3799cfe8eeb90f023ba25ef1ba4fbe05d2d
Wallace Randal <wmoura@bionexo.com>
2020-07-07 11:43:11 -0300

    improving on the code

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,14 @@
         </div>
         <hr>
         <div class="row" style="width: 48rem; margin: 0 auto">
-            <transition-group name="my-custom-flip" 
+            <transition name="my-custom-flip" 
                         enter-active-class="animate__animated animate__flipInY"
                         mode="out-in"
             >
-                <div :key="'in'" class="card" style="width: 48rem; margin: 0 auto" v-if="!selectedRightAnswer">
-                    <div class="card-header">
-                        What's 27 - 24 ?
-                    </div>
-                    <div class="card-body" >
-                        <div class="row col-12 d-flex justify-content-around">
-                            <button @click="answer(-4)" class="btn btn-primary">-4</button>
-                            <button @click="answer(3)" class="btn btn-primary">3</button>
-                        </div>
-                        <br>
-                        <div class="row col-12 justify-content-around">
-                            <button @click="answer(12)" class="btn btn-primary">12</button>
-                            <button @click="answer(14)" class="btn btn-primary">14</button>
-                        </div>
-                    </div>
-                </div>
-                <div :key="'out'" class="card text-white bg-success" style="width: 48rem; margin: 0 auto" v-else>
-                    <div class="card-body" >
-                        <div class="row col-12 d-flex justify-content-around">
-                        <h3>That's correct!</h3>
-                        </div>
-                        <br>
-                        <div class="row col-12 justify-content-around">
-                            <button @click="selectedRightAnswer = !selectedRightAnswer" class="btn btn-primary">Next Question</button>
-                        </div>
-                    </div>
-                </div>
-            </transition-group>
+                <component @right-answer="changeTo('app-answer')" 
+                           @next-question="changeTo('app-question')" 
+                           :is="mode"></component>
+            </transition>
         </div>
         
     </div>
@@ -44,22 +20,24 @@
 
 <script>
     /* eslint no-console: ["error", { allow: ["log","warn", "error"] }] */
+    import Question from "./components/Question";
+    import Answer from "./components/Answer";
     export default {
         data() {
             return {
                 rightAnswer: 27-24,
-                selectedRightAnswer: false,
+                mode: 'app-question',
             };
         },
         methods: {
-            answer (selectedNumber) {
-                if(Number(selectedNumber) !== Number(this.rightAnswer)) {
-                    alert('sorry, wrong answer');
-                } else {
-                    this.selectedRightAnswer = true;
-                }
+            changeTo(component) {
+                this.mode = component
             }
-        }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer,
+        },
     }
     
 </script>
