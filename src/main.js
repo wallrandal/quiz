03f7fb46fd7393be166fd69de66ab387409962ca
Wallace Randal <wmoura@bionexo.com>
2020-07-08 11:17:33 -0300

    finished resource exercises

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://quiz-3c2c7.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+  /* eslint no-console: ["error", { allow: ["log"] }] */
+  next();
+});
 
 new Vue({
   el: '#app',
