5f96631173202ac1f7da6a821c6512369f7acf04
Wallace Randal <wmoura@bionexo.com>
2020-10-14 07:26:54 -0300

    axios instalado e axios get implementado

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,21 +1,24 @@
+import axios from 'axios';
 import Vue from 'vue';
+// import axios from 'axios'; 
 
 /* eslint no-console: ["error", { allow: ["log"] }] */
 export const loadData = ({commit}) => {
-    Vue.http.get('data.json')
-            .then(response => {
-                if(response.data) {
-                    const stocks = response.data.stocks;
-                    const funds = response.data.funds;
-                    const stockPortfolio = response.data.stockPortfolio;
+    const url = Vue.http.options.root + '/data.json';
+    axios.get(url)        
+         .then(response => {
+            if(response.data) {
+                const stocks = response.data.stocks;
+                const funds = response.data.funds;
+                const stockPortfolio = response.data.stockPortfolio;
 
-                    const portfolio = {
-                        stocks,
-                        stockPortfolio,
-                        funds,
-                    };
-                    commit('SET_STOCKS', stocks);
-                    commit('SET_PORTFOLIO', portfolio);
-                }
-            })
+                const portfolio = {
+                    stocks,
+                    stockPortfolio,
+                    funds,
+                };
+                commit('SET_STOCKS', stocks);
+                commit('SET_PORTFOLIO', portfolio);
+            }
+         })
 }
